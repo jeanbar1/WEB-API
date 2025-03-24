60211432b9a308c72b65b1f071f4d058f0680213
Renan Magalhães <124892507+Renannz1@users.noreply.github.com>
2025-03-24 12:21:06 -0300

    fix: atualiza importacao setpasswordmixim para setpassordform em usuario/forms

--- Luni/usuario/forms.py
+++ Luni/usuario/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import SetPasswordMixin
+from django.contrib.auth.forms import SetPasswordForm
 from .models import Usuario
 
 class UsuarioForm(forms.ModelForm):
@@ -17,18 +17,28 @@ class UsuarioForm(forms.ModelForm):
 
     
 class UsuarioFormSingup(forms.ModelForm):
-    password1, password2 = SetPasswordMixin.create_password_fields()
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Digite a senha'}),
+        label="Senha"
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirme a senha'}),
+        label="Confirme a senha"
+    )
+
     class Meta:
         model = Usuario
-        fields = ['username', 'email', 'telefone', 'endereco', 'cpf',]
-        widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Digite o nome de usuário'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Digite o e-mail'}),
-            'telefone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Digite o telefone'}),
-            'endereco': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Digite o endereço'}),
-            'cpf': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Digite o CPF'}),
-            'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Digite a senha'}),
-            'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Digite a senha'}),
-        }
+        fields = ['username', 'email', 'telefone', 'endereco', 'cpf']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            self.add_error("password2", "As senhas não coincidem.")
+
+        return cleaned_data
+
 
         
